e6b2c4d77572d2e7d7829dd040c7a5dfb120aa21
djvit-iteelabs <vitaly@iteelabs.com>
2011-02-28 23:11:31 +0200

    Adjustments

--- scripts/themes.data.js
+++ scripts/themes.data.js
@@ -8,7 +8,7 @@
 
 var themesData = {
 		"themes" : [
-			{"name": "day", "start": "22.00", "end": "1.00", "theme": "style.day.css"},
-			{"name": "night", "start": "1.00", "end": "22.00", "theme": "style.night.css"}
+			{"name": "day", "start": "7.00", "end": "21.00", "theme": "style.day.css"},
+			{"name": "night", "start": "21.00", "end": "7.00", "theme": "style.night.css"}
 		]
 }
