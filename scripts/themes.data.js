d6661e3e2fed6cd66724ad9c7c22de11bb58582e
djvit-iteelabs <vitaly@iteelabs.com>
2011-03-06 12:21:47 +0200

    Events content styles update

--- scripts/themes.data.js
+++ scripts/themes.data.js
@@ -8,7 +8,7 @@
 
 var themesData = {
 		"themes" : [
-			{"name": "night", "start": "7.00", "end": "21.00", "theme": "style.night.css"},
-			{"name": "day", "start": "21.00", "end": "7.00", "theme": "style.day.css"}
+			{"name": "day", "start": "7.00", "end": "21.00", "theme": "style.day.css"},
+			{"name": "night", "start": "21.00", "end": "7.00", "theme": "style.night.css"}
 		]
 }
